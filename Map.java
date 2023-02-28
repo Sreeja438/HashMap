0e929acfff38c0e4adb3c329ac5c64d4e890be9c
Sreeja438 <125867693+Sreeja438@users.noreply.github.com>
2023-02-28 21:47:51 +0530

    Add files via upload

---
+++ Map.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+class Map {
+    public static void main(String[] args){
+        HashMap<String,Integer>map=new HashMap<String,Integer>();
+        map.put("java",2);
+        map.put("python",1);
+        map.put("c",3);
+        System.out.println(map);   
+    }
+ 
+}
+    
